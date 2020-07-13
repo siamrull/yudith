4c6de6fab7d0be01dfa98baf7d412867bd01116e
unknown <dimaslutfhi82@gmail.com>
2020-07-13 22:09:04 +0700

    fifth

--- on-admin/penjualan.php
+++ on-admin/penjualan.php
@@ -28,7 +28,7 @@
               <div class="box box-info">
                   <div class="box-body">
                   <div class="table-responsive">
-                  <table id="example2" class="table table-bordered table-striped">
+                  <table id="example1" class="table table-bordered table-striped">
                     <thead>
                       <tr>
                         <th>No</th>
